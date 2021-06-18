45693e759fa21b606dcf863d607d842fa8c4bcb5
Gendos <striker2011@gmail.com>
2021-06-18 03:44:39 +0300

    add test for add_member

--- server_test.go
+++ server_test.go
@@ -3,6 +3,7 @@ package members_club
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/Striker87/members_club/router"
@@ -26,8 +27,35 @@ func TestIndex(t *testing.T) {
 
 	handler.ServeHTTP(resp, req)
 
-	// Проверяем HTTP код
 	if resp.Code != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v", resp.Code, http.StatusOK)
+	}
+}
+
+func TestAddMemberHandler(t *testing.T) {
+	req, err := http.NewRequest("POST", "http://localhost:8080/add_member", strings.NewReader(`{"name":"test name","email":"test@test.com"}`))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	store := make(map[string]storage.User)
+	route := router.Set()
+
+	srv := Run(viper.GetString("port"), route, store)
+
+	resp := httptest.NewRecorder()
+	handler := http.HandlerFunc(srv.addMemberHandler)
+
+	handler.ServeHTTP(resp, req)
+
+	if resp.Code != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", resp.Code, http.StatusOK)
+	}
+
+	headerType := resp.Header().Get("Content-type")
+	wantHeaderType := "application/json"
+
+	if headerType != wantHeaderType {
+		t.Errorf("content type header does not match: got %v want %v", headerType, wantHeaderType)
 	}
 }
